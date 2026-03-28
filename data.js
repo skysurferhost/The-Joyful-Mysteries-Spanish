f3856a072be6af827c4ec4e3057672f0de8b66ee
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-03-28 08:36:37 -0400

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-thin-thn-truyn-tin-cho-c-b-chu-thai",
+      "name": "1. La Encarnación del Hijo de Dios",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1948400729514894,
+          "pitch": 0.05525989617790472,
+          "rotation": 0,
+          "target": "1-2-c-b-i-ving-b-thnh-isave"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.160054435162591,
+          "pitch": -0.28169234161257073,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">La Encarnación del Hijo de Dios</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">En el sexto mes, el ángel Gabriel fue enviado por Dios a una ciudad de Galilea, llamada Nazaret, a una virgen que estaba comprometida con un hombre perteneciente a la familia de David, llamado José. El nombre de la virgen era María.</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">El Angel entró en su casa y la saludó, diciendo: «¡Alégrate!, llena de gracia, el Señor está contigo». Al oír estas palabras, ella quedó desconcertada y se preguntaba qué podía significar ese saludo.</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Pero el Angel le dijo: «No temas, María, porque Dios te ha favorecido. Concebirás y darás a luz un hijo, y le pondrás por nombre Jesús; él será grande y será llamado Hijo del Altísimo. El Señor Dios le dará el trono de David, su padre, reinará sobre la casa de Jacob para siempre y su reino no tendrá fin».</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">María dijo al Angel: «¿Cómo puede ser eso, si yo no tengo relaciones con ningún hombre?».</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">El Angel le respondió: «El Espíritu Santo descenderá sobre ti y el poder del Altísimo te cubrirá con su sombra. Por eso el niño será Santo y será llamado Hijo de Dios. También tu parienta Isabel concibió un hijo a pesar de su vejez, y la que era considerada estéril, ya se encuentra en su sexto mes, porque no hay nada imposible para Dios».</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">María dijo entonces: «Yo soy la servidora del Señor, que se cumpla en mí lo que has dicho».Y el Angel se alejó.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Lucas 1:26-38)</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-b-i-ving-b-thnh-isave",
+      "name": "2. La Visitación de Nuestra Señora a su prima Santa Isabel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08240088306867044,
+          "pitch": -0.45941287007660847,
+          "rotation": 0,
+          "target": "2-3-c-b-sinh-c-cha-gisu-ni-hang-"
+        },
+        {
+          "yaw": -2.027630697812615,
+          "pitch": -0.37836001166965794,
+          "rotation": 0,
+          "target": "0-1-thin-thn-truyn-tin-cho-c-b-chu-thai"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.12524373470416705,
+          "pitch": -0.6640726414363716,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">La Visitación de Nuestra Señora a su prima Santa Isabel</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">玛利亚就在那几日起身，急速往山区去，到了犹大的一座城。她进了匝加利亚的家，就给依撒伯尔请安。依撒伯尔一听到玛利亚请安，胎儿就在她的腹中欢跃。依撒伯尔遂充满了圣神，大声呼喊说：「在女人中你是蒙祝福的，你的胎儿也是蒙祝福的。吾主的母亲驾临我这里，这是我那里得来的呢？看，你请安的声音一入我耳，胎儿就在我腹中欢喜踊跃。那信了由上主传于她的话必要完成的，是有福的。」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(路加福音 1:39-45)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-b-sinh-c-cha-gisu-ni-hang-",
+      "name": "三、耶稣基督降生",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11714081544133315,
+          "pitch": -0.15866660765025742,
+          "rotation": 0,
+          "target": "3-4-c-b-dng-c-cha-gisu-trong-n-thnh"
+        },
+        {
+          "yaw": 2.87401091767512,
+          "pitch": -0.2509765799379853,
+          "rotation": 0,
+          "target": "1-2-c-b-i-ving-b-thnh-isave"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16032498761056857,
+          "pitch": -0.4172683295906481,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣基督降生</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">那时凯撒奥古斯都出了一道上谕，叫天下的人都要登记：这是在季黎诺作叙利亚总督时，初次行的登记。于是，众人各去本城登记。若瑟因为是达味家族的人，也从加里肋亚纳匝肋城，上犹大名叫白冷的达味城去，好同自己已怀孕的聘妻玛利亚去登记。他们在那里的时候，她分娩的日期满了，便生了她的头胎男儿，用襁褓里起，放在马槽里，因为在客栈中为他们没有地方。</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">在那地区有些牧人露宿，守夜看守羊群。有上主的一个天使站在他们身边，上主的光耀环照着他们，他们便非常害怕。天使向他们说：「不要害怕！看！我给你们报告一个为全民族的大喜讯：今天在达味城中，为你们诞生了一位救世者，他是主默西亚。这是给你们的记号：你们将要看见一个婴儿，里着襁褓，躺在马槽里。」忽有一大队天军，同那天使一起赞颂天主说：</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">「天主受享光荣于高天，主爱的人在世享平安！」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">众天使离开他们往天上去了以后，牧人们就彼此说：「我们且往白冷去，看看上主报告给我们所发生的事。」他们急忙去了，找到了玛利亚和若瑟，并那躺在马槽中的婴儿。他们看见以后，就把天使对他们论这小孩所说的事，传扬开了，凡听见的人都惊讶牧人向他们所说的事。玛利亚却把这一切事默存在自己心中，反复思想。牧人们为了他们所听见和看见的一切，正如天使向他们说的一样，就光荣赞美天主回去了。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(路加福音 2:1-20)</span></p>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-b-dng-c-cha-gisu-trong-n-thnh",
+      "name": "四、圣母献耶稣于圣殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04448042282405673,
+          "pitch": -0.3609151715076102,
+          "rotation": 0,
+          "target": "4-5-c-b-tm-c-c-cha-gisu-trong-n-thnh"
+        },
+        {
+          "yaw": -2.998004314665394,
+          "pitch": -0.1812815647630721,
+          "rotation": 0,
+          "target": "2-3-c-b-sinh-c-cha-gisu-ni-hang-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.004950021509621649,
+          "pitch": -0.5050909207814644,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">圣母献耶稣于圣殿</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">按梅瑟的法律，一满了他们取洁的日期，他们便带着孩子上耶路撒冷去献给上主，就如上主的法律上所记载的：「凡开胎首生的男性，应祝圣于上主。」并该照上主法律上所吩咐的，献上祭物：一对斑鸠或两只鶵鸽。那时，在耶路撒冷有一个人，名叫西默盎。这人正义虔诚，期待着以色列的安慰，而且圣神也在他身上。他曾蒙圣神启示：自己在未看见上主的受傅者以前，决见不到死亡。他因圣神的感动，进了圣殿；那时，抱着婴孩耶稣的父母正进来，要按着法律的惯例为他行礼。西默盎就双臂接过他来，赞美天主说：</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">「主啊！现在可照你的话，放你的仆人平安去了！因为我亲眼看见了你的救援，即你在万民之前早准备好的：为作启示异邦的光明，你百姓以色列的荣耀。」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他的父亲和母亲就惊异他关于耶稣所说的这些话。西默盎祝福了他们，又向他的母亲玛利亚说：「看，这孩子已被立定，为使以色列中许多人跌倒和复起，并成为反对的记号──至于你，要有一把利剑剌透你的心灵──为叫许多人心中的思念显露出来。」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">又有一位女先知亚纳，是阿协尔支派法奴耳的女儿，已上了年纪。她出阁后，与丈夫同居了七年，以后就守寡，直到八十四岁。她斋戒祈祷，昼夜事奉天主，总不离开圣殿。正在那时刻，她也前来称谢天主，并向一切希望耶路撒冷得救赎的人，讲论这孩子。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(路加福音 2:22-38)</span></p>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-b-tm-c-c-cha-gisu-trong-n-thnh",
+      "name": "五、耶稣十二龄讲道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.224982886550988,
+          "pitch": 0.01823191313473771,
+          "rotation": 0,
+          "target": "3-4-c-b-dng-c-cha-gisu-trong-n-thnh"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.15803943551415855,
+          "pitch": -0.41004943600042054,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣十二龄讲道</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他的父母每年逾越节往耶路撒冷去。他到了十二岁时，他们又照节日的惯例上去了。过完了节日，他们回去的时候，孩童耶稣却留在耶路撒冷，他的父母并未发觉。他们只以为他在同行的人中间，遂走了一天的路程；以后，就在亲戚和相识的人中寻找他。既找不着，便折回耶路撒冷找他。</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">过了三天，才在圣殿里找到了他。他正坐在经师中，聆听他们，也询问他们。凡听见他的人，对他的智慧和对答，都惊奇不止。他们一看见他，便大为惊异，他的母亲就向他说：「孩子，为什么你这样对待我们？看，你的父亲和我，一直痛苦的找你。」耶稣对他们说：「你们为什么寻找我？你们不知道我必须在我父亲那里吗？」但是，他们不明白他对他们所说的话。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(路加福音 2:41-50)</span></p>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
